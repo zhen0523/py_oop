643efc42a94ea0875dc36d224b2ef404af339bc9
zhen0523 <zhang12zz78@hotmail.com>
2019-01-15 14:05:54 +0800

    First class

---
+++ 01.py
@@ -0,0 +1,20 @@
+class Student:
+    pass
+
+
+mingyue = Student()
+
+
+class PythonStudent:
+    name = None
+    age = 18
+    course = "python"
+
+    def dohomework(self):
+        print("我在做作业")
+
+        return None
+
+
+yueyue = PythonStudent()
+yueyue.dohomework()
